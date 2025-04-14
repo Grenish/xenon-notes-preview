d295280411443122650635e36c89c4c9525247b3
Grenish rai <107925840+Grenish@users.noreply.github.com>
2025-04-15 00:21:27 +0530

    Refactor page layout and enhance AI messaging in Home component

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,4 @@
+import { BentoPage } from "@/components/bento-page";
 import MetalLogo from "@/components/metal-logo";
 import { ScrollReveal } from "@/components/ui/ScrollReveal";
 import Image from "next/image";
@@ -23,15 +24,41 @@ export default function Home() {
           </div>
         </div>
       </div>
-      <div className="w-full min-h-screen bg-black flex">
-        <ScrollReveal>Change the way you're making notes...</ScrollReveal>
-        <Image
-          src={"/woman-notes.jpeg"}
-          alt="Hero"
-          width={1000}
-          height={1000}
-          className="w-1/2 object-cover"
-        />
+      {/* Xenon Intro 2 */}
+      <div className="w-full min-h-screen bg-black flex flex-col">
+        <section className="flex">
+          <ScrollReveal>
+            Unleash the power of AI to transform your notes into structured
+            knowledge.
+          </ScrollReveal>
+          <Image
+            src={"/woman-notes.jpeg"}
+            alt="Hero"
+            width={1000}
+            height={1000}
+            className="w-1/2 object-cover"
+          />
+        </section>
+        <section className="flex">
+          <Image
+            src={"/lib2.jpeg"}
+            alt="Hero"
+            width={1000}
+            height={1000}
+            className="w-1/2 object-cover"
+          />
+          <ScrollReveal>
+            Unlock deeper insights with yout library and accelerate your
+            understanding.
+          </ScrollReveal>
+        </section>
+      </div>
+
+      {/* what we offer */}
+      <div className="h-screen w-full bg-black flex flex-col items-center justify-center">
+        {/* <div className="">
+          <BentoPage />
+        </div> */}
       </div>
     </div>
   );
