43c8401d9af5820d2317fe1620d7cc4b5a823032
Grenish rai <107925840+Grenish@users.noreply.github.com>
2025-04-14 20:07:23 +0530

    Add Hero component with basic layout and styling

---
+++ components/hero.tsx
@@ -0,0 +1,7 @@
+export default function Hero() {
+  return (
+    <div className="w-full h-screen flex items-center justify-center bg-black">
+      <h2 className="text-white">Hero</h2>
+    </div>
+  );
+}
