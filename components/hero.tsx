69d95966195462618bee8074144fd4ab0d474d72
Grenish rai <107925840+Grenish@users.noreply.github.com>
2025-04-14 23:08:00 +0530

    Remove Hero component from the project

--- components/hero.tsx
+++
@@ -1,7 +0,0 @@
-export default function Hero() {
-  return (
-    <div className="w-full h-screen flex items-center justify-center bg-black">
-      <h2 className="text-white">Hero</h2>
-    </div>
-  );
-}
