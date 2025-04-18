a22d71a3c11a2a4984f79c1592bcaf58002f1fb6
Grenish rai <107925840+Grenish@users.noreply.github.com>
2025-04-18 16:32:43 +0530

    feat: enhance loading component layout for improved user experience

--- app/loading.tsx
+++ app/loading.tsx
@@ -1,5 +1,9 @@
 import Loader from "@/components/loader";
 
 export default function Loading() {
-  return <Loader />;
+  return (
+    <div className="flex w-full min-h-screen items-center justify-center bg-black">
+      <Loader />
+    </div>
+  );
 }
